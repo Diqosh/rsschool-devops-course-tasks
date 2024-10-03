abf2f76dee988a94517cdb2e6570ec6132d59682
Dinmukhammed Tleuzhanuly <tdimasg@gmail.com>
2024-10-03 14:21:49 +0500

    ci: second task aws

--- 2_BasicInfrastructureConfiguration/main.tf
+++ 2_BasicInfrastructureConfiguration/main.tf
@@ -18,8 +18,24 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "remote_backend" {
-  bucket = var.s3_bucket_name
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "subnets" {
+  count             = length(var.subnet_cidrs)
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
+
+  tags = {
+    Name = "Subnet-${count.index + 1}"
+    Type = count.index < 2 ? "Public" : "Private"
+  }
+}
+
+# first public subnet, first availability zone
+# second public subnet, second availability zone
+# third private subnet, first availability zone
+# fourth private subnet, second availability zone
 
--- 2_BasicInfrastructureConfiguration/varialbes.tf
+++ 2_BasicInfrastructureConfiguration/varialbes.tf
@@ -4,9 +4,17 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket for remote backend"
-  type        = string
-  default     = "remote-backend-rschool-2024q3-test"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
+variable "subnet_cidrs" {
+  description = "CIDR blocks for the subnets"
+  default     = ["10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability Zones to use for the subnets"
+  default     = ["us-west-2a", "us-west-2b"]
+}
